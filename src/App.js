c3f943eb341d404920fc4046f205b63a55eaaebb
Deepak <deepakbhimavarapu@msitprogram.net>
2022-05-05 08:21:06 +0530

    text formatted

--- src/App.js
+++ src/App.js
@@ -30,33 +30,22 @@ function App() {
         <p />
         <br />
         <p lang="en-US" className="western" align="center" style={{marginBottom: '0cm', lineHeight: '100%'}}>
-          <font face="Arial, sans-serif"><font size={2} style={{fontSize: '11pt'}}><b>Invitation
-                to P2Q4 Symposium, Oct 14, 2022, Wean Hall 5324, Carnegie Mellon
-                University</b></font></font></p>
+          <font face="Arial, sans-serif"><font size={2} style={{fontSize: '11pt'}}><b>Invitation to P2Q4 Symposium, Oct 14, 2022, Wean Hall 5324, Carnegie Mellon University</b></font></font></p>
         <p lang="en-US" align="center" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font color="#000000"><font face="Times New Roman, serif"><font face="Arial, sans-serif"><font size={5} style={{fontSize: '18pt'}}>“Pawn
-                  to Queen Four”<br />
+          <font color="#000000"><font face="Times New Roman, serif"><font face="Arial, sans-serif"><font size={5} style={{fontSize: '18pt'}}>“Pawn to Queen Four”<br />
                 </font></font></font><font color="#000000"><font face="Arial, sans-serif">50</font></font><font color="#000000"><sup><font face="Arial, sans-serif"><font size={1} style={{fontSize: '7pt'}}>th</font></font></sup></font><font color="#000000"><font face="Arial, sans-serif">
-                Anniversary Symposium of The Beginning of Speech Understanding<br />
-                and
-                the Transition from Speech Recognition to Speech Understanding</font></font></font></p>
+                Anniversary Symposium of The Beginning of Speech Understanding<br /> and the Transition from Speech Recognition to Speech Understanding</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
           <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In 1971, a committee headed by Allen Newell proposed a 5-year research effort towards the demonstration of a large vocabulary connected speech understanding system. The Harpy and Hearsay systems demonstrated at CMU in 1976 not only satisfied these goals but exceeded some of the objectives. </font></font></font>
         </p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">At
-                ARPA Cordell Green crafted a unique multi-institution research model that has proved to be very effective in organizing large-scale research efforts toward a clearly specified long-term goal.&nbsp; Green was followed by Steve Crocker who oversaw the first few years of the program.</font></font></font></p>
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">At ARPA Cordell Green crafted a unique multi-institution research model that has proved to be very effective in organizing large-scale research efforts toward a clearly specified long-term goal.&nbsp; Green was followed by Steve Crocker who oversaw the first few years of the program.</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In
-                this case, 5 institutions were each charged with demonstrating a
-                working system within 5 years satisfying the research goals laid out in the Newell report. In this model of co-opetition, the annual review meetings and presentations ensured the open sharing of ideas among the institutions.&nbsp;&nbsp;</font></font></font></p>
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In this case, 5 institutions were each charged with demonstrating a working system within 5 years satisfying the research goals laid out in the Newell report. In this model of co-opetition, the annual review meetings and presentations ensured the open sharing of ideas among the institutions.&nbsp;&nbsp;</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">The
-                first demonstration of a working Speech Understanding System happened at Carnegie Mellon in 1972 using the task domain of Voice Chess, led by the team of Raj Reddy, Lee Erman, and Victor Lesser. This event was memorialized in the Hearsay video, with a video recording of Lee Erman saying “Pawn to Queen Four”</font></font></font></p>
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">The first demonstration of a working Speech Understanding System happened at Carnegie Mellon in 1972 using the task domain of Voice Chess, led by the team of Raj Reddy, Lee Erman, and Victor Lesser. This event was memorialized in the Hearsay video, with a video recording of Lee Erman saying “Pawn to Queen Four”</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">We cordially invite you to join us at CMU on Oct 14, 2022, when we celebrate the 50</font></font><font color="#000000"><sup><font face="Arial, sans-serif"><font size={1} style={{fontSize: '7pt'}}>th</font></font></sup></font><font color="#000000"><font face="Arial, sans-serif">
-                anniversary of that demonstration of the first working Speech
-                Understanding System and to celebrate the important contribution of the ARPA SUR program under the leadership of Cordell Green and Steve Crocker. Further details will be available on </font></font><font color="#0000ff"><u><a href="http://www.P2Q4.org/"><font face="Arial, sans-serif">www.P2Q4.org</font></a></u></font><font color="#000000"><font face="Arial, sans-serif">
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">We cordially invite you to join us at CMU on Oct 14,2022, when we celebrate the 50</font></font><font color="#000000"><sup><font face="Arial, sans-serif"><font size={1} style={{fontSize: '7pt'}}>th</font></font></sup></font><font color="#000000"><font face="Arial, sans-serif"> anniversary of that demonstration of the first working Speech Understanding System and to celebrate the important contribution of the ARPA SUR program under the leadership of Cordell Green and Steve Crocker. Further details will be available on </font></font><font color="#0000ff"><u><a href="http://www.P2Q4.org/"><font face="Arial, sans-serif">www.P2Q4.org</font></a></u></font><font color="#000000"><font face="Arial, sans-serif">
               </font></font></font>
         </p>
         <p lang="en-US" className="western" style={{marginBottom: '0cm', lineHeight: '100%'}}>
