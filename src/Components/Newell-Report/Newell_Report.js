7f5792e59b7062fe1a2793d01e23c6daeff5111c
SriChandan2498 <chandan.zna7@gmail.com>
2022-05-07 12:56:41 +0530

    added button in components

--- src/Components/Newell-Report/Newell_Report.js
+++ src/Components/Newell-Report/Newell_Report.js
@@ -8,8 +8,9 @@ export default function Newell_Report()
             <div className="container" style={{fontFamily : 'western'}}>
                 <br></br>
                 <h3>SPEECH-UNDERSTANDING SYSTEMS: <br/>Final Report of a Study Group, Allen Newell, et al, CMU CSD report, May 1971</h3>
+                <br/>
+                <h5>Click on the button to download the report &nbsp;<a href="http://www.rr.cs.cmu.edu/SUR.pdf" target={"_blank"}><button className="btn btn-outline-secondary">Download</button></a></h5>
                 
-                <h5><a href="http://www.rr.cs.cmu.edu/SUR.pdf" target={"_blank"}>http://www.rr.cs.cmu.edu/SUR.pdf</a></h5>
                 <br></br>
             </div>
         </div>
