7c60bcf0d69bff2912ace7a3e49bc61aab5347c5
SriChandan2498 <chandan.zna7@gmail.com>
2022-05-07 15:58:07 +0530

    added download button in components

--- src/Components/Arpa-Internet/Arpa_Internet.js
+++ src/Components/Arpa-Internet/Arpa_Internet.js
@@ -8,7 +8,7 @@ export default function Apra_Internet()
             <div className="container" style={{fontFamily : 'western'}}>
                 <br></br>
                 <h3>Brief History of the Internet</h3>
-                <h5>Click on the button to download the report&nbsp;<a href="https://www.internetsociety.org/internet/history-internet/brief-history-internet/" target={"_blank"}><button className="btn btn-outline-secondary">Download</button></a></h5>
+                <a href="https://www.internetsociety.org/internet/history-internet/brief-history-internet/" target={"_blank"}>https://www.internetsociety.org/internet/history-internet/brief-history-internet/</a>
                 <br></br>
             </div>
         </div>
