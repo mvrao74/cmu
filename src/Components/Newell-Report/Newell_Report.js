71ccb83e99ddacece81d834d49315d01fea6a969
SriChandan2498 <chandan.zna7@gmail.com>
2022-05-09 11:38:17 +0530

    added download functionality

--- src/Components/Newell-Report/Newell_Report.js
+++ src/Components/Newell-Report/Newell_Report.js
@@ -10,7 +10,7 @@ export default function Newell_Report()
                 <h3>SPEECH-UNDERSTANDING SYSTEMS: <br/>Final Report of a Study Group, Allen Newell, et al, CMU CSD report, May 1971</h3>
                 <br/>
                 {/* "http://www.rr.cs.cmu.edu/SUR.pdf" */}
-                <h5>Click on the button to download the report &nbsp;<a href="http://www.rr.cs.cmu.edu/SUR.pdf"  target={"_blank"}><button className="btn btn-outline-secondary">Download</button></a></h5>
+                <h5>Click to download the report &nbsp;<a href="/Files/SUR.pdf"  download><button className="btn btn-outline-secondary">Download</button></a></h5>
                 <br></br>
             </div>
         </div>
