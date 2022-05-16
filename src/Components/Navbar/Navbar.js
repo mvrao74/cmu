fdc7429b8e40e4e57e26208b3cdfe8a789bf74e0
SriChandan2498 <chandan.zna7@gmail.com>
2022-05-16 19:40:57 +0530

    added backend and acknowledge page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,13 +4,13 @@ import './Navbar.css'
 export default function Navbar(){
     return(
         <div  style={{fontFamily : 'western'}}>
-            <div id="navbarTogglerDemo01">
+            <div id="navbarToggler01">
                 <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-light" id="nav_container">
                 {/* <nav class="navbar navbar-expand-sm bg-light"> */}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div  class="collapse navbar-collapse">
+                <div  class="collapse navbar-collapse" id="navbarTogglerDemo01">
                     {/* <a class="navbar-brand" href="#"><img src={img} name="Image1" align="bottom" width={215} height={35} border={0} /></a> */}
                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0"> */}
