b1e5398ca972e4879c6a50249048c00c58d404fb
SriChandan2498 <chandan.zna7@gmail.com>
2022-05-17 10:29:46 +0530

    updated home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -38,25 +38,13 @@ of the first working Speech Understanding System demonstration and the 50 years 
 Research at Carnegie Mellon University.</font></font></font>
         </p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">The first demonstration of a working Speech Understanding System happened at Carnegie Mellon
-in 1972 using the task domain of Voice Chess, led by the team of Raj Reddy, Lee Erman, and Victor
-Lesser. This event was memorialized in the Hearsay video, with a video recording of Lee Erman
-saying “Pawn to Queen Four.” This demo showed the plausibility of building a large vocabulary,
-connected speech understanding system.</font></font></font></p>
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">The first demonstration of a working Speech Understanding System happened at Carnegie Mellon in 1972 using the task domain of Voice Chess, led by the team of Raj Reddy, Lee Erman, and Victor Lesser. This event was memorialized in the
+          &nbsp;<font color="#0000ff"><u><a href="https://www.youtube.com/embed/kJ9WekPYaXQ"><font face="Arial, sans-serif">1973 Hearsay video,</font></a></u></font>  which included a detailed explanation of how the system worked using Lee Erman speaking “Pawn to Queen Four.” This demo showed the plausibility of building a large vocabulary, connected speech understanding system.</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In 1971, a committee headed by Allen Newell proposed a 5-year research effort toward the
-demonstration of such a system. Based on this report, Cordell Green at DARPA crafted a unique
-multi-institution research model that has proved to be very effective in organizing large-scale
-research efforts toward a clearly specified long-term goal. Green was followed by Steve Crocker
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In 1971, a committee headed by Allen Newell <font color="#0000ff"><u><a href="https://www.rr.cs.cmu.edu/SUR.pdf" target={"_blank"}><font face="Arial, sans-serif">proposed a 5-year research effort </font></a></u></font> toward the development and demonstration of such a system. Based on this report, Cordell Green at DARPA crafted a unique multi-institution research model. Green was followed by Steve Crocker who oversaw the first few years of the program. That model has since proved, at DARPA and elsewhere, to be highly effective in organizing large-scale research efforts toward a clearly specified long-term goal.
 who oversaw the first few years of the program.&nbsp;&nbsp;</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
-          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In this case, 5 institutions were each charged with demonstrating a working system within 5 years
-satisfying the research goals laid out in the Newell report. In this model of co-opetition, the annual
-review meetings and presentations ensured the open sharing of ideas among the institutions. The
-Harpy and Hearsay systems developed at CMU not only satisfied these goals but exceeded some
-of the objectives in 1976 at the end of the DARPA program. Building on this work and inventing
-many new paths, CMU has been at the forefront of speech understanding research for now over 50
-years.</font></font></font></p>
+          <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">In this case, five institutions were each charged with demonstrating a working system within five years satisfying the research goals laid out in the Newell report. In this model of co-opetition, the periodic review meetings and presentations ensured the open sharing of ideas among the institutions. At the end of the DARPA program, in 1976, the Harpy and Hearsay systems developed at CMU not only satisfied the original program goals but exceeded some of them.  Building on this work and inventing many new paths, CMU has been at the forefront of speech understanding research for over 50 years.</font></font></font></p>
         <p lang="en-US" style={{marginTop: '0.42cm', marginBottom: '0.42cm', lineHeight: '100%'}}>
           <font face="Times New Roman, serif"><font color="#000000"><font face="Arial, sans-serif">The 1-day symposium and celebration will include short talks and recollections of many of the key
 participants at CMU in this 50-year journey and a reception and banquet in the evening. Please visit
