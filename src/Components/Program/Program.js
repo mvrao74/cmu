b4f3fa08a1ff4366a1c747b273429ddcd3ef6f8e
vishv1098 <vishv1098@gmail.com>
2022-12-14 21:55:25 -0500

    Individual speaker videos added

--- src/Components/Program/Program.js
+++ src/Components/Program/Program.js
@@ -21,7 +21,8 @@ export default function Program()
                             <td>GHC 4405<br/>
                                 Gates and Hillman Centers</td>
                             <td>Introduction DARPA and Creation of SUS Program (Chair: Raj Reddy) <br/>
-                            Steve Crocker, Allen Sears</td>
+                            <a href="https://drive.google.com/file/d/1slYt01rDjg5XB3O0AbJzI599rPpzg2cN/view?usp=share_link">Steve Crocker</a>, 
+                            Allen Sears</td>
                         </tr>
                         <tr>
                             <td>0945</td>
@@ -36,7 +37,14 @@ export default function Program()
                             <td>GHC 4405<br/>
                                 Gates and Hillman Centers</td>
                             <td>Recollections of Early Pioneers: 1970s (Chair: Reddy)<br/>
-                            Lee Erman, Victor Lesser, Rick Roth, Jim Baker, Janet Baker, Mark Fox, Jack Mostow, Fil Alleva</td>
+                            <a href="https://drive.google.com/file/d/1zg3edTv9fvz5-m85GPB4A-R2XKl9KCfM/view?usp=share_link">Lee Erman</a>, 
+                            <a href="https://drive.google.com/file/d/1pW8LngGr5R5kPhVqPmW49xvoXqTgHPW5/view?usp=share_link">Victor Lesser</a>, 
+                            <a href="https://drive.google.com/file/d/1pW8LngGr5R5kPhVqPmW49xvoXqTgHPW5/view?usp=share_link">Rick Roth</a>, 
+                            <a href="https://drive.google.com/file/d/1qSikLKXfWH6w0PXTkeelI8UMR0-Izenz/view?usp=share_link">Jim Baker</a>, 
+                            <a href="https://drive.google.com/file/d/1ifPXqVWNtszdg1K9AdlMQESCTEtB3psz/view?usp=share_link">Janet Baker</a>, 
+                            <a href="https://drive.google.com/file/d/143oS7DbBnX2C96wK7aKKgoGTIduH4ySZ/view?usp=share_link">Mark Fox</a>, 
+                            <a href="https://drive.google.com/file/d/1rG6_k6CSKOn4bfRPhyA8O45hYSR9l1OI/view?usp=share_link">Jack Mostow</a>, 
+                            <a href="https://drive.google.com/file/d/1NcFd7uP7RB2i_cl_5Td1Cwbft7uqMLZP/view?usp=share_link">Fil Alleva</a></td>
                         </tr>
 
                         <tr>
@@ -53,7 +61,15 @@ export default function Program()
                             <td>GHC 4405<br/>
                                 Gates and Hillman Centers</td>
                             <td>Recollections of CMU Speech Group: 1980s (Chair: Richard Stern)<br/>
-                                Richard Stern, Ronald Cole, Hsiao-Wuen Hon, Wayne Ward, Roni Rosenfeld, Roberto Bisiani, Alex Waibel, Alex Acero, Bhiksha Raj</td>
+                            <a href="https://drive.google.com/file/d/1u6QZIMOO5Tycep8kMW80pc0GuI_Kc6pM/view?usp=share_link">Richard Stern</a>, 
+                            <a href="https://drive.google.com/file/d/1-eABNeS1NWz40ea0UD-s3rZDcCzxM5eL/view?usp=share_link">Ronald Cole</a>, 
+                            <a href="https://drive.google.com/file/d/1mfRDqcbFbrV1v4cizbu1-fckGaAZQytm/view?usp=share_link">Hsiao-Wuen Hon</a>, 
+                            <a href="https://drive.google.com/file/d/12eHtQDOZ0_ButgVuvR3TGc63456gF3mC/view?usp=share_link">Wayne Ward</a>, 
+                            <a href="https://drive.google.com/file/d/1XkWrTCbINMJbzKl7UpSufFcXfG11TItH/view?usp=share_link">Roni Rosenfeld</a>, 
+                            <a href="https://drive.google.com/file/d/18gOPHNefw-EjLrJ2JlqNZIyOAT7x4-sV/view?usp=share_link">Roberto Bisiani</a>, 
+                            <a href="https://drive.google.com/file/d/1NU9Cj1eOcQnEoSJHc6FFibn52Ev-fzZQ/view?usp=share_link">Alex Waibel</a>, 
+                            <a href="https://drive.google.com/file/d/1JaNSWS0Jog1apHB4UcE-iTjyKug1Ko4I/view?usp=share_link">Alex Acero</a>, 
+                            <a href="https://drive.google.com/file/d/1Qtovbmmmpg7wdGTUwP-UJOIKs67QBj3Y/view?usp=share_link">Bhiksha Raj</a></td>
                         </tr>
 
                         <tr>
@@ -63,7 +79,14 @@ export default function Program()
                                 Gates and Hillman Centers</td>
                             <td>Recollections of CMU Speech:1990s and beyond (Chair: Alex Rudnicky)
                                 <br/>
-                                Lin Chase, Florian Metze, Rita Singh, Jahanzeb Sherwani, Dan Bohus, Paul Constantinides, Matt Marge, Zhou yu</td>
+                                <a href="https://drive.google.com/file/d/17T8WdePD1hsM8u7pvUsQw64-sJdZPzku/view?usp=share_link">Lin Chase</a>, 
+                                <a href="https://drive.google.com/file/d/1_Gdr6FjGLh9fxkqVyIPfOwtR8ZNx7dUi/view?usp=share_link">Florian Metze</a>, 
+                                <a href="https://drive.google.com/file/d/1h_bD4F6Jsi2pV13WiUP_-ugJgjgM2BXu/view?usp=share_link">Rita Singh</a>, 
+                                <a href="https://drive.google.com/file/d/1olChUWxoWKXCuDvzEHBeeg9YFbadxC2I/view?usp=share_link">Jahanzeb Sherwani</a>, 
+                                <a href="https://drive.google.com/file/d/1OZy1I8Ttdoi5X7bFqMtrzHgxs5F7uZTc/view?usp=share_link">Dan Bohus</a>, 
+                                <a href="https://drive.google.com/file/d/1n3w2VkILmrpk9chAskvkvdyPdW5uxAKs/view?usp=share_link">Paul Constantinides</a>, 
+                                <a href="https://drive.google.com/file/d/14S6AqkmW9BDXCytn0clQIjVnzvrIfUPR/view?usp=share_link">Matt Marge</a>, 
+                                <a href="https://drive.google.com/file/d/1P1uN8EVhtAe20EWz9Ml-7lkxtd_NwyFD/view?usp=share_link">Zhou yu</a></td>
                         </tr>
 
                         <tr>
@@ -81,7 +104,12 @@ export default function Program()
                                 Gates and Hillman Centers</td>
                             <td>Raj Reddy AI Lecture and Panel Discussion<br/>
                                 Impact of CMU Speech Group (Chair: Harry Shum and Hsiao Wuen Hon)
-                                <br/>Janet Baker, Jim Baker, KaiFu Lee by Zoom, Alex Waibel, Xuedong Huang, Alex Acero</td>
+                                <br/><a href="https://drive.google.com/file/d/1uDtLcGElp-fUN2TuDLefgxr9YqZD05_s/view?usp=share_link">Janet Baker</a>, 
+                                <a href="https://drive.google.com/file/d/1MBp8jjutHPxuTRgxjODElmXRvV8IY9pw/view?usp=share_link">Jim Baker</a>,
+                                <a href="https://drive.google.com/file/d/18yncPuuLEtvWaxcQwhq0BfUuyAQoJpjj/view?usp=share_link">KaiFu Lee by Zoom</a>, 
+                                <a href="https://drive.google.com/file/d/1dIZ_LkjryYiLmiRLIjgBJxua4x4VQrhj/view?usp=share_link">Alex Waibel</a>, 
+                                <a href="https://drive.google.com/file/d/1w1Bd2Atoo8tmBSYAk2inzddPl8t_QQ-I/view?usp=share_link">Xuedong Huang</a>, 
+                                <a href="https://drive.google.com/file/d/1M8PuTPbHzyzIPznPFKPfKPOMX74rMEcs/view?usp=sharing">Alex Acero</a></td>
                         </tr>
                         <tr>
                             <td>1830</td>
